5e0e3f7da73975a0caa26598ce80a3b1394b7486
Ryan Schumacher <j.r.schumacher@gmail.com>
2024-08-08 04:46:43 -0500

    Add test coverage

--- staticFileServerHandler.go
+++ staticFileServerHandler.go
@@ -16,17 +16,23 @@ import (
 type staticFilesHandlerOpts struct {
 	ns            string
 	basePath      string
-	logger        slog.Logger
+	logger        *slog.Logger
 	muxErrHandler func(int) http.Handler
 	webEnv        any
 }
 
 type staticFilesHandlerFunc func(staticFilesHandlerOpts) staticFilesHandlerOpts
 
-const defaultGlobalNamespace = "APP_ENV"
+var defaultStaticFilesHandlerOpts = staticFilesHandlerOpts{
+	ns:            "APP_ENV",
+	basePath:      "/",
+	logger:        nil,
+	muxErrHandler: nil,
+	webEnv:        nil,
+}
 
 // WithLogger sets the logger for the static file server. Defaults to slog.Logger.
-func WithLogger(logger slog.Logger) staticFilesHandlerFunc {
+func WithLogger(logger *slog.Logger) staticFilesHandlerFunc {
 	return func(c staticFilesHandlerOpts) staticFilesHandlerOpts {
 		c.logger = logger
 		return c
@@ -35,6 +41,20 @@ func WithLogger(logger slog.Logger) staticFilesHandlerFunc {
 
 // WithBasePath sets the base path for the web server which will be trimmed from the request path before looking up files.
 func WithBasePath(basePath string) staticFilesHandlerFunc {
+	if basePath == "" {
+		basePath = defaultStaticFilesHandlerOpts.basePath
+	}
+
+	// ensure leading slash for trimming later
+	if basePath[0] != '/' {
+		basePath = "/" + basePath
+	}
+
+	// ensure trailing slash for trimming later
+	if basePath[len(basePath)-1] != '/' {
+		basePath = basePath + "/"
+	}
+
 	return func(c staticFilesHandlerOpts) staticFilesHandlerOpts {
 		c.basePath = basePath
 		return c
@@ -56,12 +76,13 @@ func WithMuxErrorHandler(handler func(int) http.Handler) staticFilesHandlerFunc 
 //	env: the web environment to inject, use json struct tags to drive the marshalling
 //	namespace: the namespace to use for the web environment, defaults to "APP_ENV"
 func WithInjectWebEnv(env any, namespace string) staticFilesHandlerFunc {
+	if namespace == "" {
+		namespace = defaultStaticFilesHandlerOpts.ns
+	}
+
 	return func(c staticFilesHandlerOpts) staticFilesHandlerOpts {
 		c.webEnv = env
-
-		if namespace != "" {
-			c.ns = namespace
-		}
+		c.ns = namespace
 		return c
 	}
 }
@@ -73,9 +94,7 @@ func WithInjectWebEnv(env any, namespace string) staticFilesHandlerFunc {
 //   - fn: optional functions to configure the handler (e.g. WithLogger, WithBasePath, WithMuxErrorHandler, WithInjectWebEnv)
 func StaticFilesHandler(ctx context.Context, filesys fs.FS, fn ...staticFilesHandlerFunc) (http.Handler, error) {
 	// process options
-	opts := staticFilesHandlerOpts{
-		ns: defaultGlobalNamespace,
-	}
+	opts := defaultStaticFilesHandlerOpts
 	for _, f := range fn {
 		opts = f(opts)
 	}
@@ -108,8 +127,17 @@ func StaticFilesHandler(ctx context.Context, filesys fs.FS, fn ...staticFilesHan
 			return
 		}
 
-		// clean path for security & strip leading slash to avoid FS.Open errors
-		cleanedPath := strings.TrimPrefix(path.Clean(r.URL.Path), "/")
+		// warn if base path might be wrong
+		if len(opts.basePath) > 0 && r.URL.Path[:len(opts.basePath)] != opts.basePath {
+			logWithAttrs(slog.LevelInfo, "WARNING: base path may not be set correctly",
+				slog.Attr{Key: "reqPath", Value: slog.StringValue(r.URL.Path)},
+				slog.Attr{Key: "basePath", Value: slog.StringValue(opts.basePath)},
+			)
+		}
+
+		// clean path for security and consistency
+		cleanedPath := path.Clean(r.URL.Path)
+		cleanedPath = strings.TrimPrefix(cleanedPath, opts.basePath)
 
 		// open file
 		file, err := mfilesys.Open(cleanedPath)
@@ -117,37 +145,60 @@ func StaticFilesHandler(ctx context.Context, filesys fs.FS, fn ...staticFilesHan
 			defer file.Close()
 		}
 
+		// ensure leading slash
+		r.URL.Path = cleanedPath
+		if r.URL.Path[0] != '/' {
+			r.URL.Path = "/" + r.URL.Path
+		}
+
+		// if index.html is requested, rewrite to avoid 301 redirect
+		if r.URL.Path == "/index.html" {
+			r.URL.Path = "/"
+		}
+
 		isErrNotExist := errors.Is(err, os.ErrNotExist)
 		isFile := path.Ext(cleanedPath) != ""
 
+		// if err != nil {
+		// 	fmt.Printf("error: %v\n", err)
+		// 	fmt.Printf("request path: %s\n", r.URL.Path)
+		// 	fmt.Printf("cleaned path: %s\n", cleanedPath)
+		// 	fs.WalkDir(mfilesys, ".", func(path string, d fs.DirEntry, err error) error {
+		// 		fmt.Printf("path: %s, d: %v, err: %v\n", path, d, err)
+		// 		return nil
+		// 	})
+		// }
+
 		// return 500 for other errors
 		if err != nil && !isErrNotExist {
-			logWithAttrs(slog.LevelError, "could not open file", slog.Attr{Key: "path", Value: slog.StringValue(cleanedPath)})
+			logWithAttrs(slog.LevelError, "could not open file", slog.Attr{Key: "cleanedPath", Value: slog.StringValue(cleanedPath)})
 			muxErrHandler(http.StatusInternalServerError, w, r)
 			return
 		}
 
 		// return 404 for actual static file requests that don't exist
-		if isErrNotExist && isFile {
-			logWithAttrs(slog.LevelError, "could not find file", slog.Attr{Key: "path", Value: slog.StringValue(cleanedPath)})
+		if err != nil && isErrNotExist && isFile {
+			logWithAttrs(slog.LevelError, "could not find file", slog.Attr{Key: "cleanedPath", Value: slog.StringValue(cleanedPath)})
 			muxErrHandler(http.StatusNotFound, w, r)
 			return
 		}
 
 		// serve index.html and let SPA handle undefined routes
 		if isErrNotExist {
-			logWithAttrs(slog.LevelDebug, "not found, serve index", slog.Attr{Key: "path", Value: slog.StringValue(cleanedPath)})
+			logWithAttrs(slog.LevelDebug, "not found, serve index", slog.Attr{Key: "cleanedPath", Value: slog.StringValue(cleanedPath)})
 			r.URL.Path = "/"
 		}
 
-		// let fileServer handle valid requests
 		fileServer.ServeHTTP(w, r)
 	}), nil
 }
 
 // newLoggerWithContext creates a new logger function with the given context and logger.
-func newLoggerWithContext(ctx context.Context, logger slog.Logger) func(slog.Level, string, ...slog.Attr) {
+func newLoggerWithContext(ctx context.Context, logger *slog.Logger) func(slog.Level, string, ...slog.Attr) {
 	return func(level slog.Level, msg string, attrs ...slog.Attr) {
+		if logger == nil {
+			return
+		}
 		logger.LogAttrs(ctx, level, msg, attrs...)
 	}
 }
